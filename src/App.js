ce478c3247b56ea474eb47387d275dcfdbfa01ac
Adyl uulu Nurtai <adyluulunurtai7@gmail.com.com$   git config --global user.email adyluulunurtai7@gmail.com>
2019-05-09 16:04:01 +0600

    import pages in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoomPage from "./pages/SingleRoomPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Home />
+      <Rooms />
+      <SingleRoomPage />
+    </>
+  );
 }
 
 export default App;
